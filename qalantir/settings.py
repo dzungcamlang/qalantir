588ade2ca2c91ae381301441a570f13524087467
jcpyun <johnchpyun@gmail.com>
2016-12-06 21:54:10 -0500

    edit

--- qalantir/settings.py
+++ qalantir/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ck1a!r0d!&=r+wtuqg4cv3svfxykwtj!-gnywawt1tx4%ts$-4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
